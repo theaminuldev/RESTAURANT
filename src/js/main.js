957b8a9cbd4e0923e2c57832ca7cf11956615a43
theaminuldev <mdaminulislam2015bd@gmail.com>
2024-03-10 20:35:34 +0600

    Refactor code: Delete counter.js and update main.js with new features

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,171 @@
-import '../css/style.css'
-import { setupCounter } from './counter.js'
-import javascriptLogo from '../../javascript.svg'
-import viteLogo from '/vite.svg'
+import '../css/style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+'use strict';
 
-setupCounter(document.querySelector('#counter'))
+
+
+/**
+ * PRELOAD
+ * 
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+	preloader.classList.add("loaded");
+	document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+	for (let i = 0, len = elements.length; i < len; i++) {
+		elements[i].addEventListener(eventType, callback);
+	}
+}
+
+
+/**
+ * NAVBAR
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+	navbar.classList.toggle("active");
+	overlay.classList.toggle("active");
+	document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
+/**
+ * HEADER & BACK TOP BTN
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+	const isScrollBottom = lastScrollPos < window.scrollY;
+	if (isScrollBottom) {
+		header.classList.add("hide");
+	} else {
+		header.classList.remove("hide");
+	}
+
+	lastScrollPos = window.scrollY;
+}
+
+window.addEventListener("scroll", function () {
+	if (window.scrollY >= 50) {
+		header.classList.add("active");
+		backTopBtn.classList.add("active");
+		hideHeader();
+	} else {
+		header.classList.remove("active");
+		backTopBtn.classList.remove("active");
+	}
+});
+
+
+
+/**
+ * HERO SLIDER
+ */
+
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+const heroSliderNextBtn = document.querySelector("[data-next-btn]");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSliderPos = function () {
+	lastActiveSliderItem.classList.remove("active");
+	heroSliderItems[currentSlidePos].classList.add("active");
+	lastActiveSliderItem = heroSliderItems[currentSlidePos];
+}
+
+const slideNext = function () {
+	if (currentSlidePos >= heroSliderItems.length - 1) {
+		currentSlidePos = 0;
+	} else {
+		currentSlidePos++;
+	}
+
+	updateSliderPos();
+}
+
+heroSliderNextBtn.addEventListener("click", slideNext);
+
+const slidePrev = function () {
+	if (currentSlidePos <= 0) {
+		currentSlidePos = heroSliderItems.length - 1;
+	} else {
+		currentSlidePos--;
+	}
+
+	updateSliderPos();
+}
+
+heroSliderPrevBtn.addEventListener("click", slidePrev);
+
+/**
+ * auto slide
+ */
+
+let autoSlideInterval;
+
+const autoSlide = function () {
+	autoSlideInterval = setInterval(function () {
+		slideNext();
+	}, 7000);
+}
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", function () {
+	clearInterval(autoSlideInterval);
+});
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseout", autoSlide);
+
+window.addEventListener("load", autoSlide);
+
+
+
+/**
+ * PARALLAX EFFECT
+ */
+
+const parallaxItems = document.querySelectorAll("[data-parallax-item]");
+
+let x, y;
+
+window.addEventListener("mousemove", function (event) {
+
+	x = (event.clientX / window.innerWidth * 10) - 5;
+	y = (event.clientY / window.innerHeight * 10) - 5;
+
+	// reverse the number eg. 20 -> -20, -5 -> 5
+	x = x - (x * 2);
+	y = y - (y * 2);
+
+	for (let i = 0, len = parallaxItems.length; i < len; i++) {
+		x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
+		y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
+		parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+	}
+
+});
